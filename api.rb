f9b40680ca5f06cb96802416a8d8479a59ad35fa
Jordan Elver <jordan.elver@gmail.com>
2012-03-12 11:31:06 +0000

    Initial commit

---
+++ api.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'json'
+
+class API < Sinatra::Base
+  get '/sushi.json' do
+    content_type :json
+    {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+  end
+end
